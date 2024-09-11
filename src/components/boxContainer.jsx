f58e4bf93e932fac18d94bd78b097ee346ba2ac1
Bavanuja <bbavanuja@gmail.com>
2024-09-11 13:38:02 +0530

    create participant image

--- src/components/boxContainer.jsx
+++ src/components/boxContainer.jsx
@@ -1,26 +1,24 @@
 import React from 'react'
 import "./BoxContainer.css";
 
+
 export default function BoxContainer() {
   return (
     <div className="outer-box">
         <div className="grid-container">
-            <div className="small-box">1</div>
-            <div className="small-box">2</div>
-            <div className="small-box">3</div>
-            <div className="small-box">4</div>
-            <div className="small-box">5</div>
-            <div className="small-box">6</div>
-            <div className="small-box">7</div>
-            <div className="small-box">8</div>
-            <div className="small-box">9</div>
-            <div className="small-box">9</div>
-            <div className="small-box">9</div>
-            <div className="small-box">9</div>
-            <div className="small-box">9</div>
-            <div className="small-box">9</div>
-            <div className="small-box">9</div>
-            <div className="small-box">9</div>
+            <div className="small-box">
+              <img src="/p0.jpg" alt="Participant" className="participant-image" />
+              <div className="participant-name">John Doe</div>
+              </div>
+            <div className="small-box"><img src="/p1.jpeg" alt="Participant" className="participant-image" /></div>
+            <div className="small-box"><img src="/p2.jpeg" alt="Participant" className="participant-image" /></div>
+            <div className="small-box"><img src="/p3.jpg" alt="Participant" className="participant-image" /></div>
+            <div className="small-box"><img src="/p4.jpeg" alt="Participant" className="participant-image" /></div>
+            <div className="small-box"><img src="/p5.jpeg" alt="Participant" className="participant-image" /></div>
+            <div className="small-box"><img src="/p6.jpeg" alt="Participant" className="participant-image" /></div>
+            <div className="small-box"><img src="/p7.jpg" alt="Participant" className="participant-image" /></div>
+            <div className="small-box"><img src="/p8.jpeg" alt="Participant" className="participant-image" /></div>
+            
         </div>
     </div>
 
